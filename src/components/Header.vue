aa867467e0ccd93cdce269a860f71170c0b5faa2
PaponAhasan <ahasan.papon@gmail.com>
2021-06-12 14:23:36 +0600

    Vue

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+   <div class="nav">
+       <a href="#">Home</a>
+       <a href="#">Add Restaurant</a>
+       <a href="#">Update Restaurant</a>
+       <a href="#">Logout</a>
+   </div>
+</template>
+
+<script>
+export default {
+    name:'Header'
+}
+</script>
+
+<style scoped>
+.nav{
+    background-color: #333;
+    overflow: hidden;
+}
+.nav a{
+    float: left;
+    color: white;
+    padding: 14px 16px;
+    text-align: center;
+    font-size: 17px;
+    text-decoration: none;
+    margin-right: 5px;
+}
+.nav a:hover{
+    background: #ddd;
+    color: #333;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+<Header />
+<h1>Hello User, Welcome on Home Page</h1>
+</template>
+
+<script>
+import Header from './Header.vue'
+export default {
+    name: 'Home',
+    components: {
+        Header 
+    },
+    // mounted(){
+    //   let user=localStorage.getItem('user-info');
+    //   if(!user){
+    //     this.$router.push({name:'SignUp'})
+    //   }
+    // }
+}
+</script>
